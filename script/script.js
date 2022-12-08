0259b908bebbfaa83519cf13e54d6d6c3ea7527e
Manuel Murillo <mmpeira@gmail.com>
2022-12-08 11:59:49 +0100

    formulario validado

---
+++ script/script.js
@@ -0,0 +1,41 @@
+console.log("estoy linkado");
+
+document.querySelector("form.formulario").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    console.log(event.target.name.value);
+    console.log(event.target.tlf.value);
+    console.log(event.target.email.value);
+
+    const name = event.target.name.value;
+    const tlf = event.target.tlf.value;
+    const email = event.target.email.value;
+    
+    let validated = true; 
+
+      if (name.length < 5) {
+          alert("Nombre muy corto");
+          validated = false; 
+      }
+
+      if (!email.includes("@") && !email.endsWith(".com") ) {
+          alert("Falta @ email y/o .com al final");
+          validated = false; 
+      }
+
+      if (tlf < 8) {
+          alert("telefono muy corto");
+          validated = false; 
+      }
+
+    //    Comprobación final - Todo validado
+
+      if (validated){
+          alert("ÉXITO - Formulario correcto y enviado")
+          event.target.submit();
+      }
+    
+    
+})
+
+console.log("funciono"); 
